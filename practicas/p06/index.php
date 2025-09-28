ec19d3645784559a087b64bda66a66e0a5e349f4
AdyleneBC <adylene.bc@gmail.com>
2025-09-27 19:42:34 -0600

    Ejercicio 2 resuelto

--- practicas/p06/index.php
+++ practicas/p06/index.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Práctica 4</title>
+    <?php
+    include 'src/funciones.php';
+    ?>
 </head>
+
 <body>
     <h2>Ejercicio 1</h2>
     <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
-     <?php
-        include 'src/funciones.php';
-
-        funcion1();
+    <?php
+    funcion1();
     ?>
-    
+
     <h1>Ejercicio 2</h1>
     <?php
-
-        funcion2();
+    ejercicio2();
     ?>
 
 
 </body>
+
 </html>
--- practicas/p06/src/funciones.php
+++ practicas/p06/src/funciones.php
@@ -1,38 +1,54 @@
 <?php
 
-    function funcion1(){
+/*EJERCICIO 1 */
+function funcion1()
+{
 
-        if(isset($_GET['numero']))
-        {
-            $num = $_GET['numero'];
-            if ($num%5==0 && $num%7==0)
-            {
-                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
-            }
-            else
-            {
-                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
-            }
+    if (isset($_GET['numero'])) {
+        $num = $_GET['numero'];
+        if ($num % 5 == 0 && $num % 7 == 0) {
+            echo '<h3>R= El número ' . $num . ' SÍ es múltiplo de 5 y 7.</h3>';
+        } else {
+            echo '<h3>R= El número ' . $num . ' NO es múltiplo de 5 y 7.</h3>';
+        }
+    }
+}
+
+/*Funcion para generar una matriz de M*3 y almacenar todas la iteraciones hasta llegar a que un numero sea impar, par, impar */
+/*Vamos a usar un do-while */
+
+/*EJERCICIO 2 */
+function ejercicio2() {
+    $matriz = [];   // Guardará los números en formato Mx3
+    $iteraciones = 0;
+    $secuencia = false;
+
+    while (!$secuencia) {
+        $fila = [];
+        // Generamos 3 números aleatorios
+        for ($i = 0; $i < 3; $i++) {
+            $fila[] = rand(1, 999);
+        }
+
+        // Guardamos la fila en la matriz
+        $matriz[] = $fila;
+        $iteraciones++;
+
+        // Verificamos si la ultima fila es impar, par, impar
+        if ($fila[0] % 2 != 0 && $fila[1] % 2 == 0 && $fila[2] % 2 != 0) {
+            $secuencia = true;
         }
     }
 
-    /*Funcion para generar una matriz de M*3 y almacenar todas la iteraciones hats llegar a que un numero sea impar, par, impar */
-    /*Vamos a usar un do-while */
+    // Imprimimos la matriz con comas
+foreach ($matriz as $fila) {
+    echo implode(", ", $fila) . "<br>";
+}
 
-    function funcion2 (){
-        do{
-            $val1 = rand();
-            $val2 = rand();
-            $val3 = rand();
 
-            echo $val2;
+    $total = $iteraciones * 3;
+    echo "<br>$total números obtenidos en $iteraciones iteraciones\n";
+}
 
-        }while($val2%2==0);
-        
 
-        
-        
-    }
-
-        
-    ?>
+?>
