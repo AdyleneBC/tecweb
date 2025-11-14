b83208fd9fc30d98e32a92978a96f5b466ad8d9c
AdyleneBC <adylene.bc@gmail.com>
2025-11-14 16:20:43 -0600

    Se modificó la clase DataBase, sigueindo el uml

--- practicas/p12_composer/product_app/backend/myapi/DataBase.php
+++ practicas/p12_composer/product_app/backend/myapi/DataBase.php
@@ -4,14 +4,34 @@ namespace TECWEB\MYAPI;
 
 abstract class DataBase
 {
+    // atributo protegido de conexión
     protected $conexion;
-    public function __construct($db, $user, $pass)
-    {
-        $this->conexion = @mysqli_connect('localhost', $user, $pass, $db);
 
-        //Si la conexión falló $conexión contendrá false 
+    // arreglo donde guardaremos los datos que regresan las consultas
+    protected $data;
+
+    // Constructor: DataBase(user: string, pass: string, db: string)
+    public function __construct($user, $pass, $db)
+    {
+        $this->data = array();
+
+        $this->conexion = @mysqli_connect(
+            'localhost',
+            $user,
+            $pass,
+            $db
+        );
+
+        // Si la conexión falla, $conexion será false
         if (!$this->conexion) {
             die('¡Base de datos NO conectada!');
         }
+    }
+
+    // getData(): string
+    public function getData()
+    {
+        // Regresa los datos como JSON
+        return json_encode($this->data, JSON_PRETTY_PRINT);
     }
 }
